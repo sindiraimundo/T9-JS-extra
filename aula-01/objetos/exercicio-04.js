b3e56bc0d410ac057ca56ac0c85f109e3dfa3804
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-08 14:46:31 -0300

    Reolução exercicio 04 objetos aula 01

---
+++ aula-01/objetos/exercicio-04.js
@@ -0,0 +1,11 @@
+//Crie um objeto que contenha: arrays, listas, booleanos e números. Crie no mínimo 5 propriedades.
+let cursoTurma9 = {
+    curso: ["Lógica de programação", "Bootstrap","Jquery", "JavaScript"], 
+    quantidadeAlunas: 30,
+    presencial: true,
+    ensinoDistancia: false,
+    nomeAlunas: ["Aluna1", "Aluna2", "Aluna3"],
+}
+
+console.log(cursoTurma9)
+console.log("o valor da propriedade presencial é: ", cursoTurma9.presencial)
