cfaf6e477adda8863e8b214087bb732bb31b57a9
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-08 14:24:02 -0300

    Resolução exercicio2 objetos aula 01

--- aula-01/objetos/exercicio-02.js
+++ aula-01/objetos/exercicio-02.js
@@ -1,2 +1,15 @@
 /*Crie um objeto curso que receba as propriedades: nome, período, professora.
 Atribua valores as propriedades criadas e separadamente mostre as propriedades no console.log */
+ let curso = {
+     nome: "",
+     periodo:"",
+     professora:"",
+
+ }
+
+ curso.nome = "HTML",
+ curso.periodo = "09/03/2020 a 21/04/2020",
+ curso.professora = "Eliane"
+
+console.log(curso)
+ 
