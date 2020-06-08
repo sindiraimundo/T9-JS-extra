0596cc98ead24b7513de15f9996121510ade881b
sindiraimundo <62564092+sindiraimundo@users.noreply.github.com>
2020-06-08 14:37:14 -0300

    resolução exercicio 3  objetos aula 01

---
+++ aula-01/objetos/exercicio-03.js
@@ -0,0 +1,12 @@
+/*Crie um objeto curso que receba as propriedades: nome, período, professora, nomeCurso. 
+Em seguida, escolha qual propriedade você queira acessar.*/
+
+let cursoProgramacao = {
+    nome:"Programação",
+    período:"9h às 17h",
+    professora:"Maria de Fátima",
+    nomeCurso:"Lógica de Programação",
+}
+
+console.log("O nome da Professora é: ", cursoProgramacao.professora)
+
